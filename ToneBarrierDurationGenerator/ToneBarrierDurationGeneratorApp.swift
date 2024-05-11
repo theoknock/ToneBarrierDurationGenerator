59564789e24e5c3c280d7469df4664310e958e71
James Alan Bush <jamesalanbush1972@gmail.com>
2024-05-11 06:52:39 -0400

    Initial Commit

---
+++ ToneBarrierDurationGenerator/ToneBarrierDurationGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneBarrierDurationGeneratorApp.swift
+//  ToneBarrierDurationGenerator
+//
+//  Created by Xcode Developer on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ToneBarrierDurationGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
